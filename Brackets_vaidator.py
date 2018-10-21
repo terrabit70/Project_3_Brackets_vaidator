9e21dff56b838a65801d5801fccaa47b496e6272
Terrabit70 <evgtarak@sibmail.com>
2018-10-21 22:01:11 +0700

    New validation algorythm and tests

--- Brackets_vaidator.py
+++ Brackets_vaidator.py
@@ -1,15 +1,37 @@
 print('****************************************************')
-print('*             Brackets Validator V 0.1             *')
+print('*             Brackets Validator V 0.2             *')
 print('*        It can validates only "()" brackets       *')
+print('*       It have a validate function and tests      *')
 print('****************************************************')
 
-input_string = input('Enter string to validate: ')
 
-if input_string.find('(') == -1 and input_string.find(')') == -1:
-    print('no brackets in your string')
-elif (input_string.count('(') == input_string.count(')')) and (input_string.find('(') < input_string.find(')')):
-    print('your string is valid')
-else:
-    print('Your string in not valid')
+def is_valid(text):
+    brackets_counter = 0
+    iterator = 0
+    while iterator in range(len(text)):
+        if text[iterator] == '(':
+            brackets_counter += 1
+        elif text[iterator] == ')':
+            brackets_counter -= 1
+            if brackets_counter < 0:
+                break
+        iterator += 1
+    if brackets_counter == 0:
+        string_valid = True
+    else:
+        string_valid = False
+    return string_valid
 
-input('Press Enter to exit')
+
+text_1 = '(n(o(r)m)a)s'
+text_2 = '(()))()()( zalupa'
+text_3 = ')()( zalupa'
+text_4 = '())(() zalupa'
+text_5 = '((()))'
+
+assert (is_valid(text_1))
+assert (not is_valid(text_2))
+assert (not is_valid(text_3))
+assert (not is_valid(text_4))
+assert (is_valid(text_5))
+print('SUCCESS!')
